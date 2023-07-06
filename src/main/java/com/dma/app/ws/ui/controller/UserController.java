34bd651c33ea9db9ebcbd89b8bfc0fa56fa54ff3
Diogo Almeida <diogodma80@gmail.com>
2023-07-05 20:24:58 -0400

    Using UserRest and UserDto in RestController

--- src/main/java/com/dma/app/ws/ui/controller/UserController.java
+++ src/main/java/com/dma/app/ws/ui/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.dma.app.ws.ui.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dma.app.ws.shared.dto.UserDto;
+import com.dma.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.dma.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -13,8 +19,18 @@ public class UserController {
 	
 	
 	@PostMapping
-	public String createUser() {
-		return "createUser was called";
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		
+		UserRest returnValue = new UserRest();
+		
+		UserDto userDto = new UserDto();
+		
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		return returnValue;
 	}
 	
 	@GetMapping()
