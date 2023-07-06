aed351775ee93c6b82bf1cd79c8fed5c8f368edf
Diogo Almeida <diogodma80@gmail.com>
2023-07-06 09:50:06 -0400

    Update WebSecurity and register with HttpSecurity in AuthenticationFilter class

--- src/main/java/com/dma/app/ws/security/WebSecurity.java
+++ src/main/java/com/dma/app/ws/security/WebSecurity.java
@@ -1,16 +1,18 @@
 package com.dma.app.ws.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.dma.app.ws.service.UserService;
 
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurity {
 
 	private final UserService userDetailsService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -21,16 +23,28 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-	}
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-				.permitAll() // allow post requests to /users
-				.anyRequest().authenticated(); // but requires authentication for all other requests
+		// Configure AuthenticationManagerBuilder
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		http.csrf().disable()
+		.authorizeRequests()
+			.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
+		.anyRequest()
+			.authenticated()
+		.and()
+			.authenticationManager(authenticationManager)
+			.addFilter(new AuthenticationFilter(authenticationManager));
+
+		return http.build();
+
 	}
 
 }
