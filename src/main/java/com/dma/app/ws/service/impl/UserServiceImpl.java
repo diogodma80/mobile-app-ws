34b5d0cc5cb70b17277faf0abc8e140819ff8c07
Diogo Almeida <diogodma80@gmail.com>
2023-07-05 20:34:17 -0400

    Create class UserEntity

--- src/main/java/com/dma/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/dma/app/ws/service/impl/UserServiceImpl.java
@@ -1,19 +1,8 @@
 package com.dma.app.ws.service.impl;
 
-import java.util.ArrayList;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.dma.app.ws.UserRepository;
-import com.dma.app.ws.io.entity.UserEntity;
 import com.dma.app.ws.service.UserService;
-import com.dma.app.ws.shared.Utils;
 import com.dma.app.ws.shared.dto.UserDto;
 
 @Service
